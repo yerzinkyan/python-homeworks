75ac9c020fbda6bdb0583c10b91308cea9da0956
Arman Yerzinkyan <146631181+yerzinkyan@users.noreply.github.com>
2023-12-01 22:50:58 +0400

    Add files via upload

---
+++ lab_3/main.py
@@ -0,0 +1,18 @@
+#xndir3
+import numpy as np
+vector = np.array([1, 2, 3])
+
+matrix = np.array([[4, 5, 6],
+                   [7, 8, 9],
+                   [10, 11, 12]])
+
+result = vector * matrix
+
+print("Vector:")
+print(vector)
+
+print("\nMatrix:")
+print(matrix)
+
+print("\nResult of Element-wise Multiplication:")
+print(result)
